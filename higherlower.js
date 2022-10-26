fc6654fa0c38ce2a1d91206220fe3837bc71d8d8
Juno Nguyen <junonguyen27@gmail.com>
2022-10-26 17:38:26 -0500

    added some additional features

--- higherlower.js
+++ higherlower.js
@@ -7,10 +7,12 @@ let choices= [];
 // let validInput = false;
 let message = document.getElementById("message");
 let startBtn = document.getElementById("start");
+let maxNumInput = document.getElementById("maxNum");
+let guessEl = document.getElementById("guess");
 
 function selectMax() {
     guessForm = document.getElementById("guessForm");
-    maxNum = document.getElementById("maxNum").value;
+    maxNum = maxNumInput.value;
     chosenNum = Math.ceil(Number(maxNum));
     
     if(chosenNum != NaN && maxNum > 1) {
@@ -19,6 +21,7 @@ function selectMax() {
 
         guessForm.classList = "";
         startBtn.classList = "hide";
+        maxNumInput.setAttribute('readonly', true);
     } else {
         message.innerHTML = "You need to input a number greater than 1!";
     }
@@ -26,12 +29,17 @@ function selectMax() {
 
 function do_guess() {
 
-    let guess = Number(document.getElementById("guess").value);
+    let guess = Number(guessEl.value);
 
     if(guess != NaN && guess >= 1 && guess <= chosenNum && !choices.includes(guess)) {
         if(guess == num) {
             choices.push(guess);
-            message.innerHTML = `You got it in ${choices.length} tries! Your choices were ${choices.toString()}`;
+            message.innerHTML = `You got it in ${choices.length} tries! Your choices were ${choices.toString()}. To play again, please enter a new maximum number and click start.`;
+            maxNumInput.removeAttribute('readonly');
+            choices = [];
+            guessEl.value = "";
+            guessForm.classList = "hide";
+            startBtn.classList = "btn btn-primary";
         }
         else if (guess > num) {
             message.innerHTML = "No, try a lower number.";
